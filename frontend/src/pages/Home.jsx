a5a9e40bf91959bc4c000ba6e141f9654f7ccff3
Michael Chan <110430837+michaelchan132@users.noreply.github.com>
2026-03-14 00:55:52 -0400

    Update login landing page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom"
+
 function Home() {
-    return <div>Home</div>
+    return (
+    <main>
+      <h1>Welcome</h1>
+      <p>Search restaurants, read reviews, and share your own dining experiences.</p>
+      <p>
+        <Link to="/restaurants">Browse Restaurants</Link>
+      </p>
+    </main>
+  )
 }
 
 export default Home
